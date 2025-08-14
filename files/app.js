e6fe3cc62ca4f52699638c3ea1d110624343b70c
Dani-8 <daniyalw6190@gmail.com>
2025-08-14 23:18:48 +0500

    minor update

--- files/app.js
+++ files/app.js
@@ -5,17 +5,25 @@ const dateInput = document.getElementById("date-input");
 const timeInput = document.getElementById("time-input");
 
 // TASK
+const addTaskBlock = document.getElementById("add-task-block");
+
 const taskInput = document.getElementById("task-input");
 const taskDesc = document.getElementById("task-desc");
 const addBTN = document.getElementById("add-btn");
+
 const taskList = document.getElementById("task-list");
 
 
 
+// FUNCTION TO CHECK IF DATE & TIME ARE SELECTED!!!!
+function checkSchedule(){
+    if(dateInput.value !== "" && timeInput.value !== ""){
+        addTaskBlock.style.display = "block";
+    } else {
+        addTaskBlock.style.display = "none";
+    }
 
-
-
-
+}
 
 
 
