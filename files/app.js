b14428a397a5d55470b948980f3532c3081c653a
Dani-8 <daniyalw6190@gmail.com>
2025-08-14 22:55:48 +0500

    minor update

---
+++ files/app.js
@@ -0,0 +1,73 @@
+// ALL ELEMENTS FORM HTML
+
+// TIME
+const dateInput = document.getElementById("date-input");
+const timeInput = document.getElementById("time-input");
+
+// TASK
+const taskInput = document.getElementById("task-input");
+const taskDesc = document.getElementById("task-desc");
+const addBTN = document.getElementById("add-btn");
+const taskList = document.getElementById("task-list");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
